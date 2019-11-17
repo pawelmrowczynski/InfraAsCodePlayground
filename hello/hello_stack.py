d3b9f8276e7a73ff6ad450897b19f39b10ca33c4
pawel.mrowczynski <pawel.mrowczynski@outlook.com>
2019-11-17 20:00:47 +0100

    Testing package resolution

--- hello/hello_stack.py
+++ hello/hello_stack.py
@@ -1,8 +1,5 @@
 from aws_cdk import (
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_lambda as _lambda,
     core
 )
 
@@ -13,19 +10,4 @@ class MyStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-
-        queue = sqs.Queue(
-            self, "MyFirstQueue",
-            visibility_timeout=core.Duration.seconds(300),
-        )
-
-        topic = sns.Topic(
-            self, "MyFirstTopic",
-            display_name="My First Topic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
-
-        hello = HelloConstruct(self, "MyHelloConstruct", num_buckets=4)
-        user = iam.User(self, "MyUser")
-        hello.grant_read(user)
+        _lambda.
