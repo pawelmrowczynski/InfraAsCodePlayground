2e36aaa6b246af5e310d110c39a470c489163186
pawel.mrowczynski <pawel.mrowczynski@intive.com>
2019-11-17 22:04:07 +0100

    Add a simple vpc to the stack.

--- hello/hello_stack.py
+++ hello/hello_stack.py
@@ -1,12 +1,15 @@
 from aws_cdk import (
-    aws_lambda as _lambda,
     core
 )
 
-from .hello_construct import HelloConstruct
+import aws_cdk.aws_ec2 as ec2
 
+from .hello_construct import HelloConstruct
 
 class MyStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        vpc = ec2.Vpc(self, "CloudYourselfVpc", cidr= "10.0.0.0/16")
+        
