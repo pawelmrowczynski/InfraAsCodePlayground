989057bccc18088205859f30e0e2e7d3c6b3f2cc
pawel.mrowczynski <pawel.mrowczynski@outlook.com>
2019-12-01 01:09:32 +0100

    Remove unnecessary comments.

--- aws_cdk_dotnet/src/AwsCdkDotnet/AwsCdkDotnetStack.cs
+++ aws_cdk_dotnet/src/AwsCdkDotnet/AwsCdkDotnetStack.cs
@@ -7,8 +7,7 @@ namespace AwsCdkDotnet
     {
         internal AwsCdkDotnetStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            // The CDK includes built-in constructs for most resource types, such as Queues and Topics.
-            var hello = new Function(this, "HelloHandler", new FunctionProps
+            var hello = new Function(this, "JsHelloFromDotNetCdk", new FunctionProps
             {
                 Runtime = Runtime.NODEJS_10_X, // execution environment
                 Code = Code.FromAsset("lambda"), // Code loaded from the "lambda" directory
