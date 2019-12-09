6933aacb39fa34758cff68fbe7ae42e35bfe44d3
pawel.mrowczynski <pawel.mrowczynski@outlook.com>
2019-12-09 22:10:38 +0100

    Added a first python example from aws cdk repository.

--- aws_cdk_python/app.py
+++ aws_cdk_python/app.py
@@ -4,10 +4,17 @@ from aws_cdk import core
 
 from hello.hello_stack import MyStack
 from projectOne.main import ProjectOneStack
+from aws_examples.api_cors_lambda.stack import ApiCorsLambdaStack
 
 
 app = core.App()
+#base stack that you create in first tutorial
 MyStack(app, "hello-cdk-1", env={'region': 'eu-west-1'})
+
+# my first project, see readme.md for more details
 ProjectOneStack(app, "project-one-stack", env={'region':'eu-west-1'})
+
+# first example from aws-cdk-examples: https://github.com/aws-samples/aws-cdk-examples/tree/master/python/api-cors-lambda/
+ApiCorsLambdaStack(app, "first-example-stack", env={'region':'eu-west-1'})
 
 app.synth()
---
+++ aws_cdk_python/aws_examples/readme.md
@@ -0,0 +1,6 @@
+A simple api gateway backed by a lambda.
+
+A cors is enabled for this api gateway.
+
+TODO: Try some tests. Try getting a resource that would not work with cors enabled and then try the same thing with cors enabled.
+Todo: read and test this: https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors.html
