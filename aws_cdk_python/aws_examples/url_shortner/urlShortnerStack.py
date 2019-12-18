9d0bf7f640165c895b0ae7e970e073ae9210a2ff
pawel.mrowczynski <pawel.mrowczynski@outlook.com>
2019-12-18 15:29:56 +0100

    Url shortner lambda added

--- aws_cdk_python/aws_examples/url_shortner/urlShortnerStack.py
+++ aws_cdk_python/aws_examples/url_shortner/urlShortnerStack.py
@@ -1,12 +1,23 @@
-from aws_cdk import (
-    core,
-    aws_lambda as _lambda,
-    aws_apigateway as _apigw
-)
+from aws_cdk import core, aws_dynamodb, aws_lambda, aws_apigateway
+
 
 class UrlShortnerStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-    
+        table = aws_dynamodb.Table(
+            self, 
+            "mapping-table",
+            partition_key = aws_dynamodb.Attribute(name="id", type = aws_dynamodb.AttributeType.STRING))
+        
+        function = aws_lambda.Function(
+            self, "backend",
+            runtime=aws_lambda.Runtime.PYTHON_3_7,
+            handler = "handler.main",
+            code = aws_lambda.Code.asset("aws_examples/url_shortner/lambda"))
+        table.grant_read_write_data(function)
+
+        function.add_environment("TABLE_NAME", table.table_name) ## late binding at provisioning time
+
+        api = aws_apigateway.LambdaRestApi(self, "api", handler = function)
